79fa9335fa2c8a755c26995b9e7f335c87e2cfde
maksym rozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-05-06 18:14:47 +0300

    save

---
+++ src/js/swiper.js
@@ -0,0 +1,25 @@
+import Swiper from 'swiper';
+import { Pagination, Navigation } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Swiper('.swiper', {
+    modules: [Pagination, Navigation],
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicMainBullets: 5,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
